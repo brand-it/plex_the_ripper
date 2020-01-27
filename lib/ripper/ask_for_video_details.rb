ec0b80761dc405f04736f984743ebff3b765adc3
Brandt Lareau <brandt.lareau@gmail.com>
2020-01-27 10:39:13 -0800

    Changed the Margin of min and max to 10 min

--- lib/ripper/ask_for_video_details.rb
+++ lib/ripper/ask_for_video_details.rb
@@ -68,7 +68,7 @@ class AskForVideoDetails
     runtime ||= {}
 
     # margin = config.type == :movie ? 30 : 5
-    margin = 2 # how much of wiggle room we want to give the movie times
+    margin = 10 # min how much of wiggle room we want to give the movie times
     update_minlength(runtime[:min], margin)
     update_maxlength(runtime[:max], margin)
 
