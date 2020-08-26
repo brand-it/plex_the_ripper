43c4b0a83977f1e2b66e91f66744b933f8bfab7c
Brandt Lareau <brandt.lareau@gmail.com>
2020-08-25 21:24:45 -0700

    Cleaned up the code base to make things simple

--- app/tool_box/mkv_parser.rb
+++ app/tool_box/mkv_parser.rb
@@ -11,18 +11,22 @@ module MkvParser
   PRGC = Struct.new(:code, :id, :name)
   MSG = Struct.new(:code, :flags, :count, :message, :format, :params)
 
-  def parse_mkv_string(stdout_str) # rubocop:disable Metrics/AbcSize
+  def parse_mkv_string(stdout_str)
     return [] if stdout_str.blank?
 
     stdout_str.lines.map do |line|
       type, line = line.split(':')
       line = line.strip.split(',').map { |s| s.delete('"\\') }
-      type = "MkvParser::#{type}".constantize
-      if type.new.members.size <= line.size
-        type.new(*line)
-      else
-        type.new(*line[0..(type.new.members.size - 1)], line[type.new.members.size..])
-      end
+      define_type(type, line)
+    end
+  end
+
+  def define_type(type, line)
+    type = "MkvParser::#{type}".constantize
+    if type.new.members.size <= line.size
+      type.new(*line)
+    else
+      type.new(*line[0..(type.new.members.size - 1)], line[type.new.members.size..])
     end
   end
 end
