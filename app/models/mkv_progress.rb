15622fc8ba331cf69fcc91c68df55b567287e3e6
Brandt Lareau <brandt.lareau@gmail.com>
2020-09-22 20:46:44 -0700

    Assocate the Disk Title to the progress so we can store messages on the title

--- app/models/mkv_progress.rb
+++ app/models/mkv_progress.rb
@@ -4,20 +4,20 @@
 #
 # Table name: mkv_progresses
 #
-#  id           :integer          not null, primary key
-#  completed_at :datetime
-#  failed_at    :datetime
-#  name         :string
-#  percentage   :float
-#  video_type   :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  disk_id      :integer
-#  video_id     :integer
+#  id            :integer          not null, primary key
+#  completed_at  :datetime
+#  failed_at     :datetime
+#  name          :string
+#  percentage    :float
+#  video_type    :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  disk_title_id :integer
+#  video_id      :integer
 #
 # Indexes
 #
-#  index_mkv_progresses_on_disk_id                  (disk_id)
+#  index_mkv_progresses_on_disk_title_id            (disk_title_id)
 #  index_mkv_progresses_on_video_type_and_video_id  (video_type,video_id)
 #
 class MkvProgress < ApplicationRecord
