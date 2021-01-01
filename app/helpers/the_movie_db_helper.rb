eca8790fe2d7c36259278cd5841420ab362dbb98
Brandt Lareau <brandt.lareau@gmail.com>
2021-01-01 00:48:09 -0800

    Improved colors and link desc

--- app/helpers/the_movie_db_helper.rb
+++ app/helpers/the_movie_db_helper.rb
@@ -3,19 +3,19 @@
 module TheMovieDbHelper
   HOST = 'www.themoviedb.org'
 
-  def link_to_movie_db_seasons(tv_id, season_number, text: 'view more')
+  def link_to_movie_db_seasons(tv_id, season_number, text: 'go-to the movie DB')
     link_to text, "https://#{HOST}/tv/#{tv_id}/season/#{season_number}", target: :blank
   end
 
-  def link_to_movie_db_tv(tv_id, text: 'view more')
+  def link_to_movie_db_tv(tv_id, text: 'go-to the movie DB')
     link_to text, "https://#{HOST}/tv/#{tv_id}", target: :blank
   end
 
-  def link_to_movie_db_movie(movie_id, text: 'view more')
+  def link_to_movie_db_movie(movie_id, text: 'go-to the movie DB')
     link_to text, "https://#{HOST}/movie/#{movie_id}", target: :blank
   end
 
-  def link_to_movie_db_episode(tv_id, season_number, episode_number, text: 'view more')
+  def link_to_movie_db_episode(tv_id, season_number, episode_number, text: 'go-to the movie DB')
     link_to text, "https://#{HOST}/tv/#{tv_id}/season/#{season_number}/episode/#{episode_number}", target: :blank
   end
 end
