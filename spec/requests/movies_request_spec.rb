f714a74a338fe8331ff7091449584576d5d6d40f
Brandt Lareau <brandt.lareau@gmail.com>
2021-01-05 17:53:18 -0800

    Fixed more code issues

--- spec/requests/movies_request_spec.rb
+++ spec/requests/movies_request_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.describe 'Movies', type: :request do
   let(:movie) { create :movie }
 
-  # pending "add some examples to (or delete) #{__FILE__}"
+  pending "add some examples to (or delete) #{__FILE__}"
 
-  describe 'get /movies/:id/select' do
-    subject(:select) { get select_movie_path(season) } }
-    subject(:patch_season) { patch season_url(season), params: { season: { somthing: 1 } } }
+  # describe 'get /movies/:id/select' do
+  #   subject(:select) { get select_movie_path(season) }
+  #   subject(:patch_season) { patch season_url(season), params: { season: { somthing: 1 } } }
 
-  end
+  # end
 end
