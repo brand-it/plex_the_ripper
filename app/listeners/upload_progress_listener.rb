0022d152bfd9edcd983ce4d0c85da89a9e5c656e
Brandt Lareau <blar@hey.com>
2021-10-24 16:06:25 -0700

    Trying to fix FTP 426 abort error

--- app/listeners/upload_progress_listener.rb
+++ app/listeners/upload_progress_listener.rb
@@ -13,19 +13,32 @@ class UploadProgressListener
 
   def call(chunk_size: nil)
     @completed += chunk_size
+    return if next_update.future?
 
-    component = ProgressBarComponent.new model: DiskTitle,
-                                         completed: percentage,
-                                         status: :info,
-                                         message: title
-    cable_ready[DiskTitleChannel.channel_name].morph(
+    update_component
+    @next_update = nil # clear next_update timer
+  end
+
+  def update_component
+    cable_ready[DiskTitleChannel.channel_name].morph \
       selector: "##{component.dom_id}",
       html: render(component, layout: false)
-    )
     cable_ready.broadcast
+  end
+
+  def component
+    ProgressBarComponent.new \
+      model: DiskTitle,
+      completed: percentage,
+      status: :info,
+      message: title
   end
 
   def percentage
     completed / file_size.to_f * 100
+  end
+
+  def next_update
+    @next_update ||= Time.current + 0.5
   end
 end
