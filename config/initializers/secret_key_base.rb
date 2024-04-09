0657d834dbbce00a840f25078008966af41a50e5
Brandt Lareau <brandt@lareaus.com>
2024-04-09 13:27:29 -0700

    fixed DEPRECATION WARNING: `Rails.application.secrets`
    commit-id:9f758dec

--- config/initializers/secret_key_base.rb
+++ config/initializers/secret_key_base.rb
@@ -7,7 +7,7 @@
 # This will have a few costs like for
 # example session becoming invalid on new devices and you can share cookies between multiple servers
 # However you can still use the ENV["SECRET_KEY_BASE"] and all this does not mater
-if Rails.application.secrets.secret_key_base.nil?
+if Rails.application.credentials.secret_key_base.nil?
   key_file = Rails.root.join("tmp/#{Rails.env}_secret.txt")
   unless File.exist?(key_file)
     random_key = SecureRandom.hex(64)
