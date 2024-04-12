eab0d64a5feb08fd4eb4753d9ea850f00d26b3d8
Brandt Lareau <brandt@lareaus.com>
2024-04-12 15:21:58 -0700

    Preload the disk info if possible
    commit-id:277a6cee

--- app/javascript/channels/disk_channel.js
+++ app/javascript/channels/disk_channel.js
@@ -1,13 +1,12 @@
 import CableReady from 'cable_ready'
 import consumer from './consumer'
 
-console.log("disk_channel.js")
 
 consumer.subscriptions.create('DiskTitleChannel', {
-  // print out a connection established
-  connected(data) {
-    console.log("DiskTitleChannel" + data)
-  },
+  // // print out a connection established
+  // connected(data) {
+  //   console.log("DiskTitleChannel" + data)
+  // },
   received(data) {
     if (data.cableReady) CableReady.perform(data.operations)
   }
