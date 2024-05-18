859e2a98c0621dc14b82201025155cd6f9e74eb1
Brandt Lareau <brandt@lareaus.com>
2024-05-17 17:31:58 -0700

    Make progress bar more sticky and store the progress data (#343)

--- app/listeners/mkv_progress_listener.rb
+++ app/listeners/mkv_progress_listener.rb
@@ -6,23 +6,24 @@ class MkvProgressListener
 
   delegate :render, to: :ApplicationController
 
-  attr_reader :title, :message
+  attr_reader :title, :message, :completed
 
-  option :completed, Types::Coercible::Float.optional, default: -> { 0.0 }
   option :job, Types.Instance(Job)
 
   def call(mkv_message) # rubocop:disable Metrics/MethodLength
     case mkv_message
     when MkvParser::PRGV
+      @completed ||= 0.0
       @completed = percentage(mkv_message.current, mkv_message.pmax)
       update_progress_bar
     when MkvParser::PRGT, MkvParser::PRGC
       @title = mkv_message.name
-      job.update!(metadata: job.metadata.merge(title: @title))
     when MkvParser::MSG
       store_message(mkv_message.message)
       update_message_component
     end
+
+    update_job!
   end
 
   def store_message(mkv_message)
@@ -31,7 +32,6 @@ class MkvProgressListener
     @message ||= []
     @message << mkv_message
     @message.compact_blank!
-    job.update!(metadata: job.metadata.merge(message: message.join("\n")))
   end
 
   def percentage(completed, total)
@@ -41,6 +41,19 @@ class MkvProgressListener
     return 0 if percent.nan?
 
     (percent * 100).round(2)
+  end
+
+  def update_job
+    return if next_update.future?
+
+    job.update!(
+      metadata: {
+        title:,
+        message: message.join("\n"),
+        completed:
+      }
+    )
+    @next_update = nil
   end
 
   def update_message_component
@@ -75,6 +88,6 @@ class MkvProgressListener
   end
 
   def next_update
-    @next_update ||= 0.5.seconds.from_now
+    @next_update ||= 1.second.from_now
   end
 end
