c1002517fbf6aebc980f9321144984faf8280207
Brandt Lareau <brandt@lareaus.com>
2024-09-12 13:28:05 -0700

    Improved performance of ripping titles (#451)

---
+++ app/javascript/controllers/manage_selections_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="manage-selections"
+export default class extends Controller {
+  static targets = ['input']
+
+  clearInputs() {
+    this.inputTargets.forEach(input => {
+      if (input.type === 'text') {
+        input.value = ''
+      } else if (input.type === 'checkbox' || input.type === 'radio') {
+        input.checked = false
+      } else if (input.tagName.toLowerCase() === 'select') {
+        input.selectedIndex = 0
+      }
+    })
+  }
+}
--- app/javascript/controllers/pagination_controller.js
+++ app/javascript/controllers/pagination_controller.js
@@ -1,14 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
-
+let debounce = require("lodash/debounce");
 export default class extends Controller {
   static targets = ['showMore']
+
+  initialize(){
+    this.clickTarget = debounce(this.clickTarget, 100).bind(this)
+  }
 
   connect() {
     if (!this.hasShowMoreTarget || this.showMoreTarget.dataset.disableWith === undefined) return
     window.addEventListener("scroll", () => {
-      if (this.showMoreVisable()) this.showMoreTarget.click();
+      if (this.showMoreVisable()) this.clickTarget();
     });
-    if (this.showMoreVisable()) this.showMoreTarget.click();
+    if (this.showMoreVisable()) this.clickTarget();
+  }
+
+  clickTarget() {
+    this.showMoreTarget.click();
   }
 
   showMoreVisable() {
