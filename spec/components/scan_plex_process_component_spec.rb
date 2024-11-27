f6b4c0d38e1747bf9345f2ea1701c544d3fcd2cb
Brandt Lareau <brandt@lareaus.com>
2024-11-26 16:13:33 -0800

    Removed all pending test from the output

--- spec/components/scan_plex_process_component_spec.rb
+++ spec/components/scan_plex_process_component_spec.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
-RSpec.describe ScanPlexProcessComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
-end
